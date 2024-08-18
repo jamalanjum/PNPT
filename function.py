49e77dbd9b222675e85826653d063ac13931add1
Jamal Anjum <jamalanjum@rocketmail.com>
2024-08-18 11:40:45 +0100

    functions file from TCM academy

---
+++ function.py
@@ -0,0 +1,8 @@
+#FUNCTIONs
+
+def who_am_i(): #this is a function without parameters
+    name = "Jamal" #local variable
+    age = 30
+    print("My name " + name + " and I am " + str(age) + " years old.")
+
+who_am_i()
